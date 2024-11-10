d11224ba6e833e6646edeb8e6e1b5b4a652a46db
Sonal Jaiswal <97275122+Sonal-Jaiswal@users.noreply.github.com>
2024-11-10 14:18:47 +0530

    Time: 0 ms (100%), Space: 42 MB (19.59%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,21 +2,23 @@ public class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
 
-        // Start from the end of the array
+        // Traverse the array from the last element
         for (int i = n - 1; i >= 0; i--) {
-            // If the current digit is less than 9, increment it and return the result
+            // If the current digit is less than 9, increment and return immediately
             if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
 
-            // Set the current digit to 0 and move to the next digit
+            // If the digit is 9, set it to 0 and continue to the next significant digit
             digits[i] = 0;
         }
 
-        // If we are here, it means all digits were 9 and we need an extra digit at the start
+        // If we are here, all digits were 9, so we need an extra digit at the beginning
+        // Create a new array with one extra slot, setting the first digit to 1
         int[] result = new int[n + 1];
-        result[0] = 1; // The most significant digit will be 1
+        result[0] = 1;
+
         return result;
     }
 }
