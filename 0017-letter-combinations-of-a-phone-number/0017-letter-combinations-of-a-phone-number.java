5b085e096d25282e7fd7dc3ae2d50a49b5d6b2d1
Sonal Jaiswal <vsonaljaiswal@gmail.com>
2025-06-11 23:49:41 +0530

    Time: 0 ms (100%), Space: 42.3 MB (50.65%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) return result;
+
+        // Mapping of digits to corresponding letters
+        String[] mapping = {
+            "",     // 0
+            "",     // 1
+            "abc",  // 2
+            "def",  // 3
+            "ghi",  // 4
+            "jkl",  // 5
+            "mno",  // 6
+            "pqrs", // 7
+            "tuv",  // 8
+            "wxyz"  // 9
+        };
+
+        backtrack(digits, 0, new StringBuilder(), mapping, result);
+        return result;
+    }
+
+    private void backtrack(String digits, int index, StringBuilder current, String[] mapping, List<String> result) {
+        if (index == digits.length()) {
+            result.add(current.toString());
+            return;
+        }
+
+        String letters = mapping[digits.charAt(index) - '0'];
+        for (char c : letters.toCharArray()) {
+            current.append(c);
+            backtrack(digits, index + 1, current, mapping, result);
+            current.deleteCharAt(current.length() - 1);  // backtrack
+        }
+    }
+}
