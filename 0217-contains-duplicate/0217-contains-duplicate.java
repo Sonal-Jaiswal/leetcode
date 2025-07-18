f92626c1a7c1c25e1499e4ce4d0153625b6781b9
Sonal Jaiswal <vsonaljaiswal@gmail.com>
2025-07-19 04:09:41 +0530

    Time: 13 ms (88.75%), Space: 58.6 MB (39.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,21 +1,14 @@
-public class Solution {
+import java.util.HashSet;
+
+class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> seen = new HashSet<>();
-
         for (int num : nums) {
             if (seen.contains(num)) {
-                return true; // Duplicate found
+                return true;
             }
             seen.add(num);
         }
-
-        return false; // No duplicates found
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        
-        int[] nums = {1, 2, 3, 1};
-        System.out.println(solution.containsDuplicate(nums)); // Output: true
+        return false;
     }
 }
