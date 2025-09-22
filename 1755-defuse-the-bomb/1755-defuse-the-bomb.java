5c66243fe825d8d5fde620ed6d45f6c3f2ebe804
Sonal Jaiswal <vsonaljaiswal@gmail.com>
2025-09-23 03:23:26 +0530

    Time: 3 ms (19.5%), Space: 42.4 MB (37.11%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int result[] = new int[code.length];
+        int n = code.length;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            if (k > 0) {
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n];
+                }
+                result[i] = sum;
+            } else if (k < 0) {
+                for (int j = 1; j <= -k; j++) {
+                    sum += code[(i - j + n) % n];
+                }
+                result[i] = sum;
+            } else {
+                result[i] = 0;
+            }
+        }
+        return result;
+    }
+}
