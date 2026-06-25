ef1d5bcc0650c71f31d4ebcbdd7fffc906da610f
Sonal Jaiswal <vsonaljaiswal@gmail.com>
2026-06-26 00:11:37 +0530

    Time: 0 ms (100%), Space: 43.3 MB (91%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,42 +1,68 @@
+// class Solution {
+//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+//         int m = obstacleGrid.length;
+//         int n = obstacleGrid[0].length;
+
+//         int[][] arr = new int[m][n];
+
+//         if (obstacleGrid[0][0] == 1)
+//             return 0;
+
+//         arr[0][0] = 1;
+
+//         // First column
+//         for (int i = 1; i < m; i++) {
+//             if (obstacleGrid[i][0] == 1)
+//                 arr[i][0] = 0;
+//             else
+//                 arr[i][0] = arr[i - 1][0];
+//         }
+
+//         // First row
+//         for (int j = 1; j < n; j++) {
+//             if (obstacleGrid[0][j] == 1)
+//                 arr[0][j] = 0;
+//             else
+//                 arr[0][j] = arr[0][j - 1];
+//         }
+
+//         // Remaining cells
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 if (obstacleGrid[i][j] == 1)
+//                     arr[i][j] = 0;
+//                 else
+//                     arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+//             }
+//         }
+
+//         return arr[m - 1][n - 1];
+//     }
+// }
+
+//optimised approach
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
-        int[][] arr = new int[m][n];
+        int[] dp = new int[n];
 
-        if (obstacleGrid[0][0] == 1)
-            return 0;
+        dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
 
-        arr[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-        // First column
-        for (int i = 1; i < m; i++) {
-            if (obstacleGrid[i][0] == 1)
-                arr[i][0] = 0;
-            else
-                arr[i][0] = arr[i - 1][0];
-        }
-
-        // First row
-        for (int j = 1; j < n; j++) {
-            if (obstacleGrid[0][j] == 1)
-                arr[0][j] = 0;
-            else
-                arr[0][j] = arr[0][j - 1];
-        }
-
-        // Remaining cells
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 1)
-                    arr[i][j] = 0;
-                else
-                    arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
             }
         }
 
-        return arr[m - 1][n - 1];
+        return dp[n - 1];
     }
 }
