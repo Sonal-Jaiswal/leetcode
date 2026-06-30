042a3ee83135967b04514613d8e7a07156c7820c
Sonal Jaiswal <vsonaljaiswal@gmail.com>
2026-06-30 10:00:02 +0530

    Time: 10 ms (95.55%), Space: 46.2 MB (62.27%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+
+int a = -1, b = -1, c = -1;
+int ans = 0;
+
+for (int i = 0; i < s.length(); i++) {
+    char ch = s.charAt(i);
+
+    if (ch == 'a')
+        a = i;
+    else if (ch == 'b')
+        b = i;
+    else
+        c = i;
+
+    int min = Math.min(a, Math.min(b, c));
+
+    if (min != -1)
+        ans += min + 1;
+}
+
+return ans;
+    
+}}
